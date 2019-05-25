153a59e11b79b0c45f41412d76d760435209044d
Konrad <konrad.wojciechowski97@gmail.com>
2019-05-25 19:47:25 +0200

    Dodany Program.cs

---
+++ PoznanMainStation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PoznanMainStation
+{
+    class Program
+    {
+        public static List<IRunnable> runnables = new List<IRunnable>();
+        public static List<Thread> threads = new List<Thread>();
+        static int N = 5; 
+
+        static void GenerateRunnables()
+        {
+            for (int i = 0; i<N; i++)
+            {
+
+            }
+            
+        }
+        
+        static void RunThreads()
+        {
+            foreach (IRunnable a in runnables)
+            {
+                threads.Add(new Thread(new ThreadStart(a.Run)));
+            }
+            foreach (Thread t in threads)
+            {
+               t.Start();
+            }
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+        }
+
+        [System.STAThreadAttribute()]
+        static void Main(string[] args)
+        {
+            GenerateRunnables();
+            RunThreads();
+            Console.WriteLine("test");
+            App.Main();
+        }
+    }
+}
