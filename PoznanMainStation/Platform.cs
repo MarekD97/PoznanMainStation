22f15074d77b80feeaea681a2f378915f43239f1
Konrad <konrad.wojciechowski97@gmail.com>
2019-05-26 19:53:01 +0200

    Add new class

---
+++ PoznanMainStation/Platform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoznanMainStation
+{
+    class Platform 
+    {
+        bool available;
+        int id;
+
+        public Platform(int id)
+        {
+            this.id = id;
+            this.available = true;
+        }
+
+        bool IsFree()
+        {
+            return available;
+        }
+    }
+}
